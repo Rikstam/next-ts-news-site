d84ae2206af655a967e94079c7c2750c5e1c4d87
Riku <rikukestila@gmail.com>
2021-05-27 11:08:50 +0300

    Initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Head from "next/head";
+
+const Front = () => {
+  return (
+    <>
+      <Head>
+        <title>Front page</title>
+      </Head>
+      <main>Hello world from Next!</main>
+    </>
+  );
+};
+
+export default Front;
