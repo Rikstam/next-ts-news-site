4f58aeb3da28c39ab6806c26371bc0a92cba53ae
Riku <rikukestila@gmail.com>
2021-05-28 11:33:59 +0300

    Add styles to Section

--- components/Section/style.ts
+++ components/Section/style.ts
@@ -18,4 +18,19 @@ flex-wrap: wrap; justify-content: space-between;
     }
         
 }`
-export const Title = styled.div``
+
+export const Title = styled.h2`
+  font-size: 2.8rem;
+  line-height: 1.1;
+  margin: 10px 0 15px;
+
+  @media (max-width: 800px) {
+    font-size: 2rem;
+  }
+`
+
+export const MoreLink = styled.a`
+  margin: -20px 0 30px;
+  display: inline-block;
+  vertical-align: top;
+`
