99c6d7285d49de300a1908b4d5c6f7299e83e19b
Riku <rikukestila@gmail.com>
2021-05-28 11:34:25 +0300

    Category index page

--- shared/staticPaths.ts
+++ shared/staticPaths.ts
@@ -1,4 +1,4 @@
-import { EntityId } from "./types"
+import { EntityId, Category } from "./types"
 type PostStaticParams = {
     id: EntityId
 }
@@ -10,4 +10,22 @@ const staticPostsIdList: EntityId[] = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 export const postPaths: PostStaticPath[] = staticPostsIdList.map((id) => ({
     params: { id: String(id) }
 })
+)
+
+type CategoryStaticParams = {
+    id: Category
+}
+
+type CategoryStaticPath = {
+    params: CategoryStaticParams
+}
+
+const categoriesToPreRender: Category[] = [
+    "Science",
+    "Technology",
+    "Arts"
+]
+
+export const categoryPaths: CategoryStaticPath[] = categoriesToPreRender.map(
+    (category) => ({ params: { id: category } })
 )
