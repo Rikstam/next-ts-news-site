69d78d430f18a1265ad2c7bf5d77798232425a1a
Riku <rikukestila@gmail.com>
2021-05-29 12:17:17 +0300

    API endpoint for comment submit

--- api/comments.ts
+++ api/comments.ts
@@ -1,9 +1,21 @@
 import fetch from "node-fetch"
-import { EntityId } from "../shared/types"
+import { EntityId, Person } from "../shared/types"
 import { config } from "./config"
 
 export async function fetchComments(postId: EntityId
 ): Promise<Comment[]> {
     const res = await fetch(`${config.baseUrl}/comments/${postId}`)
     return await res.json()
+}
+
+export async function submitComment(
+    postId: EntityId,
+    name: Person,
+    comment: string
+): Promise<Response> {
+    return await fetch(`${config.baseUrl}/posts/${postId}/comments`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json;charset=utf-8" },
+        body: JSON.stringify({ name, comment })
+    })
 }
